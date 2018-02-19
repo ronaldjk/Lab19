c435593df8feb3521e291294ec5167edd1950433
ronaldjk <ronaldjiwonkim@gmail.com>
2018-02-19 00:36:51 -0500

    first commit lab19

---
+++ shoppingCart.js
@@ -0,0 +1,126 @@
+var shoppingCart = (function () {
+    // Methods and properties that are private
+    var cart = []; // var cart is an array.
+
+    function Item(name, price, count) {
+        this.name = name
+        this.price = price
+        this.count = count
+    }
+
+    function saveCart() {
+        localStorage.setItem("shoppingCart", JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        cart = JSON.parse(localStorage.getItem("shoppingCart"));
+        if (cart === null) {
+            cart = []
+        }
+    }
+
+    loadCart();
+
+
+
+    // Methods and properties that are public
+    var obj = {};
+
+    obj.addItemToCart = function (name, price, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count += count;
+                saveCart();
+                return;
+            }
+        }
+
+        console.log("addItemToCart:", name, price, count);
+
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    };
+
+    obj.setCountForItem = function (name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.removeItemFromCart = function (name) { //This removes only one item
+        for (var i in cart) {
+            if (cart[i].name === name) { 
+                cart[i].count--; 
+                if (cart[i].count === 0) {
+                    cart.splice(i, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.removeItemFromCartAll = function (name) { // This removes all of one item
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart.splice(i, 1);
+                break;
+            }
+        }
+        saveCart();
+    };
+
+
+    obj.clearCart = function () {
+        cart = [];
+        saveCart();
+    }
+
+
+    obj.countCart = function () { // This returns the total count
+        var totalCount = 0;
+        for (var i in cart) {
+            totalCount += cart[i].count;
+        }
+
+        return totalCount;
+    };
+
+    obj.totalCart = function () { // This returns the total cost
+        var totalCost = 0;
+        for (var i in cart) {
+            totalCost += cart[i].price * cart[i].count;
+        }
+        return totalCost.toFixed(2);
+    };
+
+    obj.listCart = function () { 
+        var cartCopy = [];
+        console.log("Listing cart");
+        console.log(cart);
+        for (var i in cart) {
+            console.log(i);
+            var item = cart[i];
+            var itemCopy = {};
+            for (var p in item) {
+                itemCopy[p] = item[p];
+            }
+            itemCopy.total = (item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy);
+        }
+        return cartCopy;
+    };
+
+    
+    return obj;
+})();
+
+
+
